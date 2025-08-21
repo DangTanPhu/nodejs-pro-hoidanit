323000e2eb19ea4fb12a34e3fa89cd25d1367854
DangTanPhu <phudt777@gmail.com>
2025-08-22 00:26:08 +0700

    Complete product and detail for client

--- src/controllers/client/product.controller.ts
+++ src/controllers/client/product.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express-serve-static-core"
+import { getProductById } from "services/client/item.service";
 const getProductPage = async (req:Request,res:Response)=> {
-    return res.render("client/product/detail.ejs");
+    const {id} =req.params;
+    const product=await getProductById(+id);
+    return res.render("client/product/detail.ejs",{
+        product
+    });
 }
 export {
     getProductPage
