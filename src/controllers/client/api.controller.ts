1e93033d3d8368cf1b8ea6c13e2059e9809ea122
DangTanPhu <phudt777@gmail.com>
2025-09-25 14:48:21 +0700

    Complete chapter 14 : Video 166

--- src/controllers/client/api.controller.ts
+++ src/controllers/client/api.controller.ts
@@ -1,24 +1,72 @@
 import { Request, Response } from "express-serve-static-core"
-import { handleGetAllUser } from "services/client/api.service";
+import { handleDeleteUserById, handleGetAllUser, handleGetUserById, handleUpdateUserById } from "services/client/api.service";
+import { registerNewUser } from "services/client/auth.service";
 import { addProductToCart } from "services/client/item.service";
+import { RegisterSchema, TRegisterSchema } from "src/validation/register.schema";
 
-const postAddProductToCartAPI = async(req:Request, res:Response)=>{
-    const {quantity,productId} = req.body;
-    const user= req.user;
-    const currentSum = req?.user?.sumCart ?? 0 ;
+const postAddProductToCartAPI = async (req: Request, res: Response) => {
+    const { quantity, productId } = req.body;
+    const user = req.user;
+    const currentSum = req?.user?.sumCart ?? 0;
     const newSum = currentSum + (+quantity);
-    await addProductToCart(+quantity,+productId,user);
+    await addProductToCart(+quantity, +productId, user);
     res.status(200).json({
-        data :newSum
+        data: newSum
     })
 }
-const getAllUsersAPI = async(req:Request, res:Response)=>{
-    const users= await handleGetAllUser();
+const getAllUsersAPI = async (req: Request, res: Response) => {
+    const users = await handleGetAllUser();
     res.status(200).json({
-        data :users
+        data: users
     })
+}
+const getAllUserByIdAPI = async (req: Request, res: Response) => {
+    const { id } = req.params
+    const users = await handleGetUserById(+id);
+    res.status(200).json({
+        data: users
+    })
+}
+const createUsersAPI = async (req: Request, res: Response) => {
+    const { fullName, email, password } = req.body as TRegisterSchema;
+    const validate = await RegisterSchema.safeParseAsync(req.body);
+    if (!validate.success) {
+        const errorZod = validate.error.issues;
+        const errors = errorZod?.map(item => `${item.message} (${item.path[0]})`);
+        
+        res.status(400).json({
+            errors:errors
+        })   
+        return;
+    }
+    await registerNewUser(fullName, email, password);
+    res.status(201).json({
+            data: "create user succeed"
+        })
+}
+
+const updateUserByIdAPI = async (req: Request, res: Response) => {
+    const { fullName, address, phone } = req.body ;
+    const {id} = req.params;
+
+    // if(fullname) => update fullname : neu dung patch thi kiem tra nghiem ngat, khi truyen cai nao thi se update cai do
+    await handleUpdateUserById(+id,fullName, address, phone);
+    res.status(200).json({
+            data: "Update user succeed"
+        })
+}
+const deleteUserByIdAPI = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    await handleDeleteUserById(+id);
+    res.status(200).json({
+            data: "Delete user succeed"
+        })
 }
 export {
     postAddProductToCartAPI,
-    getAllUsersAPI
+    getAllUsersAPI,
+    getAllUserByIdAPI,
+    createUsersAPI,
+    updateUserByIdAPI,
+    deleteUserByIdAPI
 }
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,16 @@
-import { getAllUsersAPI, postAddProductToCartAPI } from 'controllers/client/api.controller';
+import { createUsersAPI, deleteUserByIdAPI, getAllUserByIdAPI, getAllUsersAPI, postAddProductToCartAPI, updateUserByIdAPI } from 'controllers/client/api.controller';
 import express, { Express } from 'express'
 const router = express.Router();
 const apiRouter = (app: Express) => {
     router.post('/add-product-to-cart', postAddProductToCartAPI);
-    router.get('/get-all-users', getAllUsersAPI);
+    router.get('/users', getAllUsersAPI);
+    router.get('/users/:id', getAllUserByIdAPI);
+    router.post('/users',createUsersAPI);
+    // put and patch : su dung nao cung duoc ko khac nhau , khac nhau ve logic thoi
+    // put la thuong map toan bo, gui day du field
+    // patch kiem tra tung field neu co , chi gui 1-2 field
+    router.put('/users/:id',updateUserByIdAPI);
+    router.delete('/users/:id',deleteUserByIdAPI);
     app.use('/api', router)
 }
 export default apiRouter;
